17474aefd893fa93e9723d815458eac02704f7e8
Juan Domínguez <juan@buziapp.com>
2018-07-09 11:18:45 -0500

    VideoListItem populated from YouTube!

---
+++ src/components/video_list.js
@@ -0,0 +1,20 @@
+import React from 'React';
+import PropTypes from 'prop-types';
+import VideoListItem from './video_list_item';
+
+const VideoList = (props) => {
+  const videoItems= props.videos.map(video => {
+    return <VideoListItem key={video.etag} video={video} />
+  });
+  return (
+    <ul className="col-md-4 list-group">
+      { videoItems }
+    </ul>
+  );
+};
+
+VideoList.propTypes = {
+  videos: PropTypes.array
+};
+
+export default VideoList;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 
 const API_KEY = 'AIzaSyCpcfZny9QrHv_QoC5BB8kfgsavY0PPzbA';
 
-// Create a new component. This component should produce some html
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({ key: API_KEY, term: 'surfboards' }, (videos) => {
+      this.setState({ videos });
+    });
+  }
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoList videos={this.state.videos} />
+      </div>
+    );
+  }
 }
 
 
